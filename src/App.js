aa77d1bf74d7e9ae4553a891924590aa5208bf95
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-01-20 00:06:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import FirstPage from './components/main-page/MainPage';
+import FirstPage from './components/loginAndCreateAccount/FirstPage';
 import MainPage from './components/main-page/MainPage';
 import classes from './App.module.css';
 
@@ -7,10 +7,9 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   
  useEffect(() => {
-   const loggedIn = localStorage.getItem('login');
+   const loggedIn = localStorage.getItem('loggedIn');
    if(loggedIn === 1){
        setIsLoggedIn(true);
-       alert('hshsh');
    };
  }, []);
 
