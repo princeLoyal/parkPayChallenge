aa3fdeed67ece01a7444ee77ea29add7670add63
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-01-19 23:15:06 +0100

    Update MainPage.js

--- src/components/main-page/MainPage.js
+++ src/components/main-page/MainPage.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import Header from "./Header";
 import Transaction from "./Transaction/Transaction";
+import TransactionList from "./Transaction/TransactionList";
 const MainPage = () => {
     return (
         <Fragment>
