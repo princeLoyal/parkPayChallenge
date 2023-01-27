f4b82a52614861c317351b3d4e52c36529c15dca
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-01-27 20:30:19 +0100

    Update Profile.js

--- src/components/main-page/Profile.js
+++ src/components/main-page/Profile.js
@@ -12,7 +12,7 @@ import contactUs from '../../assests/mdi-light_phone.png';
 import classes from './Profile.module.css';
 const Profile = props => {
     return (
-        <div style={{height:90vh}}>
+        <div>
             <div className={classes['profile-fDiv']}>
                 <div>
                     <p>My Account</p>
