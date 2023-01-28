c342054ab9795f55361e3e51dc0df31f6063809c
princeLoyal <98643554+princeLoyal@users.noreply.github.com>
2023-01-28 19:20:44 +0100

    Update CreateAccount.js

--- src/components/loginAndCreateAccount/CreateAccount.js
+++ src/components/loginAndCreateAccount/CreateAccount.js
@@ -30,7 +30,7 @@ const CreateAccount = props => {
       const databaseHandler = async() => {
          const response = await fetch('https://park-pay-a96b2-default-rtdb.firebaseio.com/user.json');
          const data1 = await response.json();
-         await alert(data1);
+         await alert(data1[0]);
       };
       databaseHandler();
       //localStorage.setItem('loggedIn', 1);
